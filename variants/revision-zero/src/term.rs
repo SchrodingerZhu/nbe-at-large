76ea3da94635a359321a4a83e835ec4e9d2d384a
Schrodinger ZHU Yifan <yifanzhu@rochester.edu>
2022-11-27 16:02:43 -0500

    stage

--- variants/revision-zero/src/term.rs
+++ variants/revision-zero/src/term.rs
@@ -1,5 +1,5 @@
 use grammar::syntactic::{ParseTree, Ptr};
-use std::{rc::Rc, collections::HashMap};
+use std::{collections::HashMap, rc::Rc};
 
 #[derive(Clone, Debug)]
 #[repr(transparent)]
@@ -28,8 +28,12 @@ pub enum Term {
     Pi(RcPtr<Self>, Name, RcPtr<Self>),
     Ann(RcPtr<Self>, RcPtr<Self>),
     Let(Name, RcPtr<Self>, RcPtr<Self>),
+    TrustMe,
+    BottomType,
+    BottomElim,
     UnitType,
     UnitIntro,
+    UnitElim(RcPtr<Self>),
     BoolType,
     BoolIntro(bool),
     BoolElim(RcPtr<Self>, RcPtr<Self>, RcPtr<Self>),
@@ -46,66 +50,75 @@ type Results<'a, T> = (
 struct SyntaxContext<'a> {
     source_name: &'a str,
     source: &'a str,
-    variables: HashMap<&'a str, Name>
+    variables: HashMap<&'a str, Name>,
 }
 
 impl<'a> SyntaxContext<'a> {
-    fn new(source_name: &'a str,
-            source: &'a str) -> Self {
-                Self {
-                    source_name,
-                    source,
-                    variables: HashMap::new()
-                }
-    }   
-    fn init_error(&self, offset: usize) -> ariadne::ReportBuilder<(&'a str, std::ops::Range<usize>)> 
-    {
+    fn new(source_name: &'a str, source: &'a str) -> Self {
+        Self {
+            source_name,
+            source,
+            variables: HashMap::new(),
+        }
+    }
+    fn init_error(
+        &self,
+        offset: usize,
+    ) -> ariadne::ReportBuilder<(&'a str, std::ops::Range<usize>)> {
         ariadne::Report::build(ariadne::ReportKind::Error, self.source_name, offset)
-    } 
-    fn init_warning(&self, offset: usize) -> ariadne::ReportBuilder<(&'a str, std::ops::Range<usize>)> 
-    {
+    }
+    fn init_warning(
+        &self,
+        offset: usize,
+    ) -> ariadne::ReportBuilder<(&'a str, std::ops::Range<usize>)> {
         ariadne::Report::build(ariadne::ReportKind::Warning, self.source_name, offset)
-    } 
+    }
 }
 
 impl Term {
-    fn new_from_definition<'a>(ctx: &mut SyntaxContext<'a>, tree: &Ptr<ParseTree<'_>>) -> Results<'a, RcPtr<Self>> {
+    fn new_from_expr<'a>(
+        ctx: &mut SyntaxContext<'a>,
+        tree: &Ptr<ParseTree<'_>>,
+    ) -> Results<'a, RcPtr<Self>> {
+        todo!()
+    }
+    fn new_from_definition<'a>(
+        ctx: &mut SyntaxContext<'a>,
+        tree: &Ptr<ParseTree<'_>>,
+    ) -> Results<'a, RcPtr<Self>> {
         use ariadne::*;
         match tree.data.as_ref() {
-            ParseTree::Literal(_) => todo!(),
-            ParseTree::Implicit(_) => todo!(),
             ParseTree::Import(_) => {
-                let report = ctx.init_warning(tree.location.start)
+                let report = ctx
+                    .init_warning(tree.location.start)
                     .with_message("unsupported feature")
-                    .with_label(Label::new((ctx.source_name, tree.location.clone())).with_color(Color::Yellow).with_message("import is not supported and will be ignored"))
+                    .with_label(
+                        Label::new((ctx.source_name, tree.location.clone()))
+                            .with_color(Color::Yellow)
+                            .with_message("import is not supported and will be ignored"),
+                    )
                     .finish();
                 (None, vec![report])
-            },
-            ParseTree::Module { .. } => unreachable!(),
-            ParseTree::Underscore => todo!(),
-            ParseTree::Variable { name, annotation } => todo!(),
-            ParseTree::Type => todo!(),
-            ParseTree::TypeExpr { name, params } => todo!(),
-            ParseTree::Telescope { name, annotation } => todo!(),
-            ParseTree::Arrow { lhs, rhs } => todo!(),
-            ParseTree::TypeDecl {
-                former,
-                constructors,
-            } => todo!(),
-            ParseTree::TypeFormer { name, params } => todo!(),
-            ParseTree::Constructor { name, r#type } => todo!(),
-            ParseTree::FuncDecl { name, r#type } => todo!(),
-            ParseTree::FuncDefine { name, params, body } => todo!(),
-            ParseTree::FuncApply { func, args } => todo!(),
-            ParseTree::Branch { r#if, then, r#else } => todo!(),
-            ParseTree::PatternRule {
-                constructor,
-                variables,
-                body,
-            } => todo!(),
-            ParseTree::PatternMatch { expr, rules } => todo!(),
-            ParseTree::Let { var, binding, body } => todo!(),
-            ParseTree::Lambda { params, body } => todo!(),
+            }
+            ParseTree::TypeDecl { .. } => {
+                let report = ctx
+                    .init_warning(tree.location.start)
+                    .with_message("unsupported feature")
+                    .with_label(
+                        Label::new((ctx.source_name, tree.location.clone()))
+                            .with_color(Color::Yellow)
+                            .with_message("custom data type is not supported and will be ignored"),
+                    )
+                    .finish();
+                (None, vec![report])
+            }
+            ParseTree::FuncDecl { name, r#type } => {
+                todo!()
+            }
+            ParseTree::FuncDefine { name, params, body } => {
+                todo!()
+            }
+            _ => unreachable!(),
         }
     }
 
@@ -145,10 +158,15 @@ fn test() {
     module Test
     import A
     import B
+    data List (a : Type) = {
+        Nil : List a;
+        Cons : a -> List a -> List a;
+    }
 "#;
     let parse_tree = grammar::syntactic::parse(source).0.unwrap();
     let module = Term::new_from_module("source.txt", source, &parse_tree);
     for i in module.1.iter() {
-        i.eprint(("source.txt", ariadne::Source::from(source))).unwrap();
+        i.eprint(("source.txt", ariadne::Source::from(source)))
+            .unwrap();
     }
 }
