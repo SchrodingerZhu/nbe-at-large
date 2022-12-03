1290c5e1320b5023980e479639f6ee7916c676d7
Schrodinger ZHU Yifan <i@zhuyi.fan>
2022-12-02 23:21:05 -0500

    more

--- variants/revision-zero/src/term.rs
+++ variants/revision-zero/src/term.rs
@@ -646,10 +646,91 @@ impl Term {
 }
 
 struct EvaluationContext {
-    // TODO: fix this
+    // local: HashMap<Name, RcPtr<Term>>,
+    // remote: HashMap<Name, RcPtr<Term>>
 }
 
 impl Term {
+    fn alpha_equal(&self, other: &Self) -> bool {
+        struct UnificationContext(UnsafeCell<HashMap<Name, Name>>);
+        struct Guard<'a>(&'a UnificationContext, Name);
+        impl UnificationContext {
+            fn unify(&self, x: Name, y: Name) -> Guard {
+                unsafe { (*self.0.get()).insert(x.clone(), y) };
+                Guard(self, x)
+            }
+            fn check(&self, x: &Name, y: &Name) -> bool {
+                unsafe { (*self.0.get()).get(x).map(|x| x == y).unwrap_or(false) }
+            }
+        }
+        impl<'a> Drop for Guard<'a> {
+            fn drop(&mut self) {
+                unsafe {
+                    (*self.0 .0.get()).remove(&self.1);
+                }
+            }
+        }
+        fn alpha_equal_impl(a: &Term, b: &Term, ctx: &UnificationContext) -> bool {
+            match (a, b) {
+                (Term::Type, Term::Type)
+                | (Term::TrustMe, Term::TrustMe)
+                | (Term::BottomType, Term::BottomType)
+                | (Term::UnitType, Term::UnitType)
+                | (Term::UnitIntro, Term::UnitIntro)
+                | (Term::BoolType, Term::BoolType) => true,
+                (Term::Variable(x), Term::Variable(y)) => ctx.check(x, y),
+                (Term::Lam(ax, ay), Term::Lam(bx, by)) => {
+                    let _guard = if let (Some(ax), Some(bx)) = (ax, bx) {
+                        Some(ctx.unify(ax.clone(), bx.clone()))
+                    } else {
+                        None
+                    };
+                    alpha_equal_impl(ay, by, ctx)
+                }
+                (Term::App(ax, ay), Term::App(bx, by))
+                | (Term::Pi(ax, ay), Term::Pi(bx, by))
+                | (Term::UnitElim(ax, ay), Term::UnitElim(bx, by))
+                | (Term::SigmaType(ax, ay), Term::SigmaType(bx, by))
+                | (Term::SigmaIntro(ax, ay), Term::SigmaIntro(bx, by)) => {
+                    alpha_equal_impl(ax, bx, ctx) && alpha_equal_impl(ay, by, ctx)
+                }
+                (Term::Ann(ax, _), Term::Ann(bx, _)) => alpha_equal_impl(ax, bx, ctx),
+                (Term::Let(ax, ay, az), Term::Let(bx, by, bz)) => {
+                    let _guard = if let (Some(ax), Some(bx)) = (ax, bx) {
+                        Some(ctx.unify(ax.clone(), bx.clone()))
+                    } else {
+                        None
+                    };
+                    alpha_equal_impl(ay, by, ctx) && alpha_equal_impl(az, bz, ctx)
+                }
+                (Term::BottomElim(ax), Term::BottomElim(bx)) => alpha_equal_impl(ax, bx, ctx),
+                (Term::BoolIntro(ax), Term::BoolIntro(bx)) => ax == bx,
+                (Term::BoolElim(ax, ay, az), Term::BoolElim(bx, by, bz)) => {
+                    alpha_equal_impl(ax, bx, ctx)
+                        && alpha_equal_impl(ay, by, ctx)
+                        && alpha_equal_impl(az, bz, ctx)
+                }
+                (Term::SigmaElim(a0, a1, a2, a3), Term::SigmaElim(b0, b1, b2, b3)) => {
+                    alpha_equal_impl(a0, b0, ctx) && {
+                        let _guard1 = if let (Some(ax), Some(bx)) = (a1, b1) {
+                            Some(ctx.unify(ax.clone(), bx.clone()))
+                        } else {
+                            None
+                        };
+                        let _guard2 = if let (Some(ax), Some(bx)) = (a2, b2) {
+                            Some(ctx.unify(ax.clone(), bx.clone()))
+                        } else {
+                            None
+                        };
+                        alpha_equal_impl(a3, b3, ctx)
+                    }
+                }
+                _ => false,
+            }
+        }
+        let ctx = UnificationContext(UnsafeCell::new(HashMap::new()));
+        alpha_equal_impl(self, other, &ctx)
+    }
     fn whnf(ctx: &EvaluationContext, tree: RcPtr<Self>) -> RcPtr<Self> {
         match tree.data.as_ref() {
             Term::Type => tree,
@@ -674,13 +755,14 @@ impl Term {
             }
             Term::Pi(_, _) => tree,
             Term::Ann(x, _) => x.clone(),
-            Term::Let(x, a, b) => {
-                let b = match x {
-                    Some(x) => Term::instantiate(b.clone(), [(x.clone(), a.clone())].into_iter()),
-                    None => b.clone(),
-                };
-                Term::whnf(ctx, b)
-            }
+            Term::Let(x, y, z) => match x {
+                Some(name) => {
+                    let body =
+                        Term::instantiate(z.clone(), [(name.clone(), y.clone())].into_iter());
+                    Term::whnf(ctx, body)
+                }
+                None => Term::whnf(ctx, z.clone()),
+            },
             Term::TrustMe => tree,
             Term::BottomType => tree,
             Term::BottomElim(_) => tree,
@@ -1106,7 +1188,10 @@ mod test {
                         body.clone(),
                         [(name.clone().unwrap(), target)].into_iter(),
                     );
-                    println!("{}", Term::whnf(&super::EvaluationContext {}, result))
+                    assert_eq!(
+                        "(True , True)",
+                        format!("{}", Term::whnf(&super::EvaluationContext {}, result))
+                    )
                 }
                 _ => unreachable!(),
             },
