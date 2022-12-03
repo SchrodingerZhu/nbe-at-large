8da90b8478112a89f07a03a96effd8bb4f805066
Schrodinger ZHU Yifan <i@zhuyi.fan>
2022-12-03 17:39:49 -0500

    stage

--- variants/revision-zero/src/equalization.rs
+++ variants/revision-zero/src/equalization.rs
@@ -1,10 +1,36 @@
+use crate::alpha_equality::AlphaEquality;
 use crate::term::{Name, RcPtr, Term};
-use std::collections::HashMap;
+use crate::typecheck::TypeCheckContext;
 
-trait Equlization: Sized {
+trait Equalization: Sized {
     type Name;
     type Wrapper<T>;
-    type Context;
+    type Context<'a>;
 
-    fn equalize(x: &Self::Wrapper<Self>, y: &Self::Wrapper<Self>, ctx: &Self::Context) -> bool;
+    fn equalize<'a>(
+        x: &Self::Wrapper<Self>,
+        y: &Self::Wrapper<Self>,
+        ctx: &Self::Context<'a>,
+    ) -> bool;
+}
+
+impl Equalization for Term {
+    type Name = Name;
+
+    type Wrapper<T> = RcPtr<T>;
+
+    type Context<'a> = TypeCheckContext<'a>;
+
+    fn equalize<'a>(
+        x: &Self::Wrapper<Self>,
+        y: &Self::Wrapper<Self>,
+        ctx: &Self::Context<'a>,
+    ) -> bool {
+        if x.data.as_ref().alpha_equal(y.data.as_ref()) {
+            return true;
+        }
+        match (x.data.as_ref(), y.data.as_ref()) {
+            _ => todo!(),
+        }
+    }
 }
